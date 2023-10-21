4cb468b8e28745760da65de6dde28aca817ab627
dima <dimapen57@gmail.com>
2023-10-21 23:20:57 +0100

    changes999

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://mern-pizza-app.onrender.com"],
   })
 );
 app.get("/", (req, res) => {
